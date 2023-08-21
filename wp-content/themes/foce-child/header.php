9e17b508bed0658838f1b76251fca66ad75a5ae4
David BEAUMONT <dadid333@gmail.com>
2023-08-21 09:36:53 +0200

    maj header

--- wp-content/themes/foce-child/header.php
+++ wp-content/themes/foce-child/header.php
@@ -17,7 +17,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
 	<?php wp_head(); ?>
 </head>
 
